34e2a40b1b01d433bedcf0953e81fa3dfe934148
darlantc <darlantc@me.com>
2014-04-11 11:21:49 -0300

    First Relase
    First Commit

---
+++ woocommerce-alert-existing-sku.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ Plugin Name: WooCommerce Alert Existing SKU
+ Version: 1.0
+ Plugin URI: https://github.com/darlantc/WooCommerce-Alert-Existing-SKU
+ Description: WooCommerce Addon to verify if the new SKU is unique 
+ Author: Darlan ten Caten
+ Author URI: http://www.i9solucoesdigitais.com.br
+ ----
+
+ /**
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
