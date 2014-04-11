824916f95a2822405704fabb2fa138b7c9259312
darlantc <darlantc@me.com>
2014-04-11 19:48:18 -0300

    Beta
    Beta version ready for testing

--- woocommerce-alert-existing-sku.php
+++ woocommerce-alert-existing-sku.php
@@ -28,3 +28,58 @@
  * **********************************************************************
  */
 
+/******************************
+* Includes
+******************************/
+include('includes/scripts.php'); // this controls all JS / CSS   
+
+/*************************************/
+/* Adding AJAX function */
+/*************************************/
+add_action( 'init', 'waes_ajax_function' );
+function waes_ajax_function() {
+    add_action('wp_ajax_waes_load_sku', 'waes_load_sku');
+}
+
+/*************************************/
+/* Functions */
+/*************************************/
+function waes_load_sku () {
+	$valid = true;
+	$message = __( 'Código de REFERÊNCIA válido.' );
+
+    $new_sku = $_POST['newSKU'];
+
+    $args = array(
+        'post_type' => 'product',
+        'meta_key' => '_sku',
+        'meta_query' => array(
+            'relation' => 'AND',
+                array(
+                    'key' => '_sku',
+                    'value' => $new_sku,
+                    'compare' => 'LIKE'
+                )
+        ),
+        'posts_per_page'   =>  10,
+        'post_status' => 'publish',
+        'orderby'     => 'title', 
+        'order'       => 'ASC'
+    );
+
+    $sku_list = array();
+
+    $posts = get_posts( $args );
+    foreach ($posts as $post) {
+    	$meta = get_post_meta( $post->ID, '_sku', true );
+    	$sku_list[] = $meta;
+    	if ( strtolower( $new_sku ) == strtolower( $meta ) ) {
+    		$valid = false;
+    		$message = __( 'Código de REFERÊNCIA já existe. Não é permitido duplicar.' );
+    	}
+    } 
+    $result = array( 'valid' => $valid, 'message' => $message, 'sku_list' => $sku_list );
+
+    echo json_encode($result);
+    die();
+}
